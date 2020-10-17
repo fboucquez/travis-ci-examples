238e808e103289efb5e3556f5a92119d685ae2be
fernando <fboucquez@gmail.com>
2020-10-17 08:38:34 -0300

    testing

--- travis/publish.sh
+++ travis/publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+. ./travis/travis-functions.sh
+load_version_from_file()
 echo "Publishing version $VERSION"
 bash ./publish-me.sh one "$OPERATION"
--- travis/release.sh
+++ travis/release.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+. ./travis/travis-functions.sh
+load_version_from_file()
 echo "Releasing version $VERSION"
 bash ./release-me.sh one "$OPERATION"
